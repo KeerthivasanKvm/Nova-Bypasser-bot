654aed022e6e54f7efb10f31fe2a9ce39acda960
KeerthivasanKvm <160764239+KeerthivasanKvm@users.noreply.github.com>
2025-12-14 14:20:44 +0530

    Create __init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+from . import start, admin, bypass, subscription
+
+def register_handlers(app: Client):
+    """Register all bot handlers"""
+    # This function is called from main.py to register handlers
+    # Handlers are registered via decorators in their respective files
+    pass
